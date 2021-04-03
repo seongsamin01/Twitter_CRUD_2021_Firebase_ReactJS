5de6e80e14d5bea0388b5dc0ba2288c2bbce5e71
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-03 16:09:46 +0900

    첫페이지 #Log out
    Navigation
    Redirect
    react-router-dom
    Link to
    
    Navigation 파일을 만들어준다.
    log out되면 home페이지로 돌아가게 만들었다.

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,11 @@
 import React from "react";
+import { authService } from "fbase";
 
-export default () => <span>Profile</span>
+export default () => {
+    const onLogOutClick = () => authService.signOut();
+    return (
+        <>
+          <button onClick={onLogOutClick}>Log Out</button>
+        </>  
+    );
+};
