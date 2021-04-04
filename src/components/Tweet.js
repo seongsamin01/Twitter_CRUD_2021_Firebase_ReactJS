0324f3560c51818b0bda5fd960d5b065180adbb7
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-04 14:17:18 +0900

    #Tweet 메세지 수정하기 (CRUD)
    **delete 기능은 전 commit에서 끝냄
    1.tweet edit버튼 누르면 수정input창이 뜨게하고, update button이 뜨도록 만들어준다.
    
    2. input창에 실제로 타이핑이 되지 않기 때문에 타이핑 되게 만들어준다.
    
    3. update cancel버튼을 만들어준다.
    
    4. update버튼을 누르면 수정된 내용이 윈도우창에 뜨게 출력코드를 입력해준다.

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,15 +1,60 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-const Tweet = ({ tweetObj, isOwner }) => (
-    <div>
-        <h4>{tweetObj.text}</h4>
-        {isOwner && (
-          <>
-            <button>Delete Tweet</button>
-            <button>Edit Tweet</button>
-          </>  
-        )}
+const Tweet = ({ tweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newTweet, setNewTweet] = useState(tweetObj.text);
+    const onDeleteClick = async () => {
+      const ok = window.confirm("정말로 이 tweet을 지우겠습니까?");
+      console.log(ok);
+      if(ok) {
+        await dbService.doc(`tweets/${tweetObj.id}`).delete();
+      }
+    };
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`tweets/${tweetObj.id}`).update({
+            text: newTweet,
+        });
+        setEditing(false);
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewTweet(value);
+    };
+    return (
+      <div>
+        {editing ? (
+            <>
+            <form onSubmit={onSubmit}>
+                <input 
+                    type="text"
+                    placeholder="Edit your Tweet"
+                    value={newTweet}
+                    required
+                    onChange={onChange}
+                />
+                <input type="submit" value="update Tweet" />
+            </form>
+            <button onClick={toggleEditing}>Cancel</button>
+            </>
+        ) : (
+        <>
+            <h4>{tweetObj.text}</h4>
+            {isOwner && (
+                <>
+                <button onClick={onDeleteClick}>Delete Tweet</button>
+                <button onClick={toggleEditing}>Edit Tweet</button>
+                </>  
+            )} 
+        </>
+      )}
     </div>
-);
+  );
+};
+
 
 export default Tweet;
