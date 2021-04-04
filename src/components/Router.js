64ed623ad110b71234f79dec9a18cdceb26901cb
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-04 18:22:00 +0900

    #Tweet 프로필수정하기 3
    실시간으로 profile username 적용시키기

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,7 @@ import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
 
-const AppRouter = ({ isLoggedIn, userObj }) => {
+const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
     
     return (
         <Router>
@@ -17,7 +17,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
                     <Home userObj={userObj} />
                 </Route>
                 <Route exact path="/profile">
-                    <Profile userObj={userObj} />
+                    <Profile userObj={userObj} refreshUser={refreshUser} />
                 </Route>
                 </>
             ) : (
